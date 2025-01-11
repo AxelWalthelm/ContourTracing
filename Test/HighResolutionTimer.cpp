2ac760aec2f495a0507b711993c0569eaf623032
Axel <>
2025-01-11 14:18:39 +0100

    Update HighResolutionTimer

--- Test/HighResolutionTimer.cpp
+++ Test/HighResolutionTimer.cpp
@@ -19,7 +19,7 @@ HighResolutionTime_t GetHighResolutionTime()
 uint64_t GetHighResolutionTimeElapsedNs(HighResolutionTime_t start, HighResolutionTime_t stop)
 {
 	if (stop < start)
-		throw new std::overflow_error("negative time");
+		throw std::overflow_error("negative time");
 
 	uint64_t ticks = stop - start;
 	LARGE_INTEGER frequency;
@@ -30,7 +30,7 @@ uint64_t GetHighResolutionTimeElapsedNs(HighResolutionTime_t start, HighResoluti
 		return ticks * n / f;
 
 	if (f >= UINT64_MAX / n)
-		throw new std::overflow_error("timer resolution too high"); // consider to use 128 bit division
+		throw std::overflow_error("timer resolution too high"); // consider to use 128 bit division
 	uint64_t seconds = ticks / f;
 	uint64_t nanoseconds = (ticks - seconds * f) * n / f;
 	return seconds * n + nanoseconds;
@@ -42,5 +42,35 @@ uint64_t GetHighResolutionTimeElapsedNs(HighResolutionTime_t start)
 }
 
 #else
-#error "TODO: implement HighResolutionTimer"
+
+#include <iostream>
+#include <time.h>
+#include <stdexcept>
+
+HighResolutionTime_t GetHighResolutionTime()
+{
+	constexpr uint64_t n = 1000000000;
+	timespec t;
+    clock_gettime(CLOCK_BOOTTIME, &t);
+	// The combined value may overflow, but when using unsigned arithmetic the C++ standard asserts that
+	// it will wrap around at maximum value, so it still can be used for valid 64 bit time differences.
+	return uint64_t((uint64_t)t.tv_sec * n + (uint64_t)t.tv_nsec);
+}
+
+uint64_t GetHighResolutionTimeElapsedNs(HighResolutionTime_t start, HighResolutionTime_t stop)
+{
+	static_assert(std::is_same<HighResolutionTime_t, uint64_t>::value, "HighResolutionTime_t is not uint64_t");
+
+	uint64_t diff = stop - start;
+	if ((int64_t)diff < 0)
+		throw std::overflow_error("negative time");
+
+	return diff;
+}
+
+uint64_t GetHighResolutionTimeElapsedNs(HighResolutionTime_t start)
+{
+	return GetHighResolutionTimeElapsedNs(start, GetHighResolutionTime());
+}
+
 #endif
